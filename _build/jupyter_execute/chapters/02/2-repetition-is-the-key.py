c2f4182347380b1833685a985aca9188e84f72d5
Benedikt Zoennchen <zoennchen.benedikt@hm.edu>
2021-10-11 12:18:48 +0200

    make md executable (1).

--- _build/jupyter_execute/chapters/02/2-repetition-is-the-key.py
+++ _build/jupyter_execute/chapters/02/2-repetition-is-the-key.py
@@ -1,15 +1,3 @@
----
-jupytext:
-  formats: md:myst
-  text_representation:
-    extension: .md
-    format_name: myst
-kernelspec:
-  display_name: Python 3
-  language: python
-  name: python3
----
-
 # Wiederholung als Grundlage
 
 [Computational Thinking](sec-what-is-ct) beginnt normalerweise dort wo alle Denkprozesse beginnen: in den Köpfen der Menschen.
@@ -37,12 +25,11 @@ Solange sich eine Problemlösung (unsere Musikkomposition) durch Wiederholung au
 Die Komplexität des Programmcodes (die länge des Stücks) entkoppelt sich von der Komplexität der ausgeführten Berechnung (der Aufführung).
 Zum Beispiel beschreibt folgender ``Python``-code die Berechnung der Summe aller Quadratzahlen von ``1`` bis ``n+1``.
 
-```{code-cell} python3
 def sum_sqrt(n):
     sum_sqrt = 0
     for i in range(n):
         sum_sqrt += (i+1) * (i+1)
     return sum_sqrt
-```
+
 Die Anzahl der auszuführenden Anweisungen steigt linear mit ``n``, doch der Programmiercode bleibt unverändert.
 Hierin liegt die ganze Magie: **Wiederholung**!
