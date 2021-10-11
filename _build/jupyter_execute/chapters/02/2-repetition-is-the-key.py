95187cf0eeca278d58f442507ccbc0aec5b5c539
Benedikt Zoennchen <zoennchen.benedikt@hm.edu>
2021-10-11 14:24:39 +0200

    no comment

--- _build/jupyter_execute/chapters/02/2-repetition-is-the-key.py
+++ _build/jupyter_execute/chapters/02/2-repetition-is-the-key.py
@@ -5,3 +5,6 @@ def sum_sqrt(n):
     for i in range(n):
         sum_sqrt += (i+1) * (i+1)
     return sum_sqrt
+
+Die Anzahl der auszuführenden Anweisungen steigt linear mit ``n``, doch der Programmiercode bleibt unverändert.
+Hierin liegt die ganze Magie: **Wiederholung**!
